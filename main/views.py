58109e9804d849ce7356773d7446026e0c8e7a8a
jordanhutahaean <joan09go@gmail.com>
2026-09-16 03:17:05 +0700

    feat: Membuat ProjectForm menggunakan ModelForm

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from main.models import Experience, Education
 
@@ -29,3 +33,17 @@ def show_education(request):
         "education_list": Education.objects.all().order_by('-start_year'),
     }
     return render(request, "education.html", context)
+
+def create_project(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Proyek baru berhasil ditambahkan!")
+        return redirect("main:show_projects")
+
+    context = {
+        "name": "Burhan",
+        "form": form,
+    }
+    return render(request, "projects_form.html", context)
